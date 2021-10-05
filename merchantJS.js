01043f64e5758b3208b9c5176a02315a812e9703
KimrAndrew <andrewrk1997@gmail.com>
2021-10-05 10:33:36 -0700

    table now renders new rows when form is submitted

--- merchantJS.js
+++ merchantJS.js
@@ -7,10 +7,30 @@ let productSubmit = function(event) {
     let details = newProduct.details.value;
     let invQTY = newProduct.inventoryQuantity.value;
     newProduct = new Products(name,url,price,invQTY);
-    console.log(newProduct);
+    addToTable(newProduct);
     return newProduct;
 }
 
+
 let productForm = document.getElementById("add-new-item");
 productForm.addEventListener("submit",productSubmit);
+
+let tbody = document.querySelector('tbody');
+
+const addToTable = function(productsObject) {
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    td.innerHTML = productsObject.productName;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = productsObject.productPrice;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = productsObject.productQuantity;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = '<button>Remove Item</button>'
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+}
 
